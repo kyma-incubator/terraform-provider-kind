cc37ab9a7cd4321ce3964c1656f442c86b7eed95
Daniel Roth <daniel.broetchen@gmail.com>
2020-07-16 06:56:19 -0700

    Wait for ready (#15)
    * add wait_for_ready
    
    * Update resource_kind.go

--- example/main.tf
+++ example/main.tf
@@ -3,6 +3,7 @@ provider "kind" {}
 resource "kind" "my-cluster" {
     name = "test-cluster"
     node_image = "kindest/node:v1.18.4"
+    wait_for_ready = true
     kind_config =<<KIONF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
