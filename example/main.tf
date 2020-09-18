1f63cea7dad517631f0fc2bf61883f251d7732dd
Daniel Roth <daniel.roth02@sap.com>
2020-09-18 10:02:08 -0700

    update example and docs

--- example/main.tf
+++ example/main.tf
@@ -2,18 +2,24 @@ provider "kind" {
 }
 
 resource "kind_cluster" "default" {
-  name           = "test-cluster"
-  node_image     = "kindest/node:v1.19.1"
+  name = "new-cluster"
   wait_for_ready = true
   kind_config {
-    kind       = "Cluster"
-    api_version = "kind.x-k8s.io/v1alpha4"
-    nodes {
-      role = "control-plane"
-    }
-    nodes {
-      role = "worker"
-    }
+	kind = "Cluster"
+	api_version = "kind.x-k8s.io/v1alpha4"
+
+	nodes {
+		role = "control-plane"
+	}
+
+	nodes {
+		role = "worker"
+		image = "kindest/node:v1.19.1"
+	}
+
+	nodes {
+		role = "worker"
+	}
   }
 }
 
